7ca7f133104b10b1774a0237cbbf2756f8479680
joshiprashanthd <58854114+joshiprashanthd@users.noreply.github.com>
2019-12-19 22:30:55 +0530

    Changing Hidden Activation to Tanh and it is working delightful. Prediction are correct for about 98%

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
+"""
+@author: Prashant Joshi
+@date-time: 12-19-2019  10:27 PM
+"""
+
 from NeuralNetwork import NeuralNetwork
-from activation import Sigmoid, Tanh
+from activation import Sigmoid, Tanh, ReLu
 
 import numpy as np
 
 X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 y = np.array([0, 1, 1, 0])
 
+
+
 if __name__ == "__main__":
     nn = NeuralNetwork(input_units=2, hidden_units=2, output_units=1,
-                       hidden_activation=Sigmoid, output_activation=Sigmoid, learning_rate=0.1)
+                       hidden_activation=Tanh, output_activation=Sigmoid, learning_rate=0.1)
 
+    epochs = 10000
+    print("Initial Prediction")
     for i in range(4):
         print(f"y{i}: {nn.predict(X[i].reshape(1, 2))}")
 
-    for i in range(500):
+    for i in range(epochs):
         for j in range(4):
             nn.train(X[j].reshape(1, 2), y[j])
 
+    print("\nAfter training for {} epochs".format(epochs))
     for i in range(4):
         print(f"y{i}: {nn.predict(X[i].reshape(1, 2))}")
