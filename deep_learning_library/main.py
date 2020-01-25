e0186904da8ad2bf58da0f6cb5e82d8261cdc572
joshiprashanthd <58854114+joshiprashanthd@users.noreply.github.com>
2020-01-26 00:11:55 +0530

    Trained IRIS Dataset on the model. Getting loss of 8-10

--- deep_learning_library/main.py
+++ deep_learning_library/main.py
@@ -9,41 +9,41 @@ import activations
 import loss
 import optimizers
 
-# from sklearn.datasets import load_iris
-# from sklearn.model_selection import train_test_split
-# from keras.utils import to_categorical
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
 
-# dataset = load_iris()
+dataset = load_iris()
 
-# X = dataset.data.reshape((-1, 4, 1))
-# y = dataset.target
+X = dataset.data.reshape((-1, 4, 1))
+y = dataset.target
 
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# X_train = X_train.reshape((-1, 4, 1))
-# X_test = X_test.reshape((-1, 4, 1))
-# y_train = to_categorical(y_train)
-# y_train = y_train.reshape((-1, 3, 1))
-# y_test = to_categorical(y_test)
-# y_test = y_test.reshape((-1, 3, 1))
+X_train = X_train.reshape((-1, 4, 1))
+X_test = X_test.reshape((-1, 4, 1))
+y_train = to_categorical(y_train)
+y_train = y_train.reshape((-1, 3, 1))
+y_test = to_categorical(y_test)
+y_test = y_test.reshape((-1, 3, 1))
 
-X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]]).reshape((-1, 2, 1))
-y = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, 1]]).reshape((-1, 3, 1))
+# X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]]).reshape((-1, 2, 1))
+# y = np.array([[1, 0], [0, 1], [0, 1], [1, 0]]).reshape((-1, 2, 1))
 
 if __name__ == "__main__":
     
     model = models.Sequential()
-    model.add(layers.Dense(2, activation = activations.ReLu(), input_shape=X.shape[1:], weight_init_method="uniform"))
-    # model.add(layers.Dense(4, activation=activations.ReLu()))
-    model.add(layers.Dense(3, activation=activations.Softmax()))
+    model.add(layers.Dense(16, activation = activations.Tanh(), input_shape=X_train.shape[1:]))
+    model.add(layers.Dense(16, activation=activations.Tanh()))
+    model.add(layers.Dense(3, activation=activations.Sigmoid()))
     
-    model.compile(loss=loss.CategoricalCrossEntropy(), optimizer=optimizers.SGD(lr=0.1, momentum=0))
+    model.compile(loss=loss.MSE(), optimizer=optimizers.SGD(lr=0.1, momentum=0))
     
-    # print("Initial Prediction: ")
-    # print(model.predict(X))
-    
-    model.fit(X, y, batch_size=4, epochs=100, shuffle=True)
-    
-    # print(y_test[:10])
-    # print(X_train[:10])
-    print(model.predict(X))
+    model.fit(X_train, y_train, batch_size=32, epochs=500, shuffle=True)
+
+    print(model.predict(X_test[0:10]))
+    print(y_test[:10])
+
+# arr = np.array([1, 4, 4, 5]).reshape((-1, 1, 1))
+# print(arr)
+# print(np.hstack([arr, arr]))
